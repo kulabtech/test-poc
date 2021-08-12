a02a43e1be58da9cf7997741ae9dc785a4fab35e
kiran_ankani <kiran-ankani>
2021-08-12 17:49:30 +0530

    route added

--- src/App.js
+++ src/App.js
@@ -93,6 +93,7 @@ function App() {
             <Route path="/sign-in" exact component={() =><SignIn Login={login} GLogin={gLogin} />}/>
             <Route path="/sign-up" exact component={SignUp} />
             <Route path="/forgot-password" exact component={ForgotPassword} />
+            <Route exact path='/userlist' exact component={UserList} />
           </Switch>
           </HashRouter>
         </div>
